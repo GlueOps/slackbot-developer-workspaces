d97bf122af8c8a5322f2a133e187b4a2d15fbcb2
NichArchA82 <64152648+NichArchA82@users.noreply.github.com>
2024-09-01 20:36:41 +0000

    feat: added vm button command

--- command-handler/src/cmd-handler/buttons.js
+++ command-handler/src/cmd-handler/buttons.js
@@ -1,9 +1,12 @@
 const registeredButtons = {
     button_list_servers: {
-        command: 'button',
+        command: 'vm',
     },
     button_create_vm: {
-        command: 'button',
+        command: 'vm',
+    },
+    button_delete_vm: {
+        command: 'vm',
     },
     button_click: {
         command: 'button',
--- command-handler/src/commands/vm.js
+++ command-handler/src/commands/vm.js
@@ -4,21 +4,79 @@ import 'dotenv/config';
 export default {
     description: 'Sets up vm options',
 
+    button: async ({ handler, actionId, body, response }) => {
+        if (actionId === 'button_list_servers') {
+            const data = await axios.get('https://api.hetzner.cloud/v1/servers', {
+                headers: {
+                  'Authorization': `Bearer ${process.env.HETZNER_API_TOKEN}`
+                }
+              })
+              .catch(error => {
+                console.error('Error:', error);
+              });
+    
+              console.log(data.data);
+    
+            response({
+                text: `${JSON.stringify(data.data)}`
+            })
+        } else if (actionId === 'button_create_vm') {
+            response({
+                text: `This is a placeholder Button to create vms`
+            });
+        } else if (actionId === 'button_delete_vm') {
+            response({
+                text: `This is a placeholder Button to delete vms`
+            });
+        } else {
+            response({
+                text: `This button is registered with the vm command, but does not have an action associated with it.`
+            })
+        }
+    },
+    
     run: async ({ response }) => {
 
-        const data = await axios.get('https://api.hetzner.cloud/v1/servers', {
-            headers: {
-              'Authorization': `Bearer ${process.env.HETZNER_API_TOKEN}`
-            }
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-
-          console.log(data.data);
-
         response({
-            text: `${JSON.stringify(data.data)}`
-        })
+            blocks: [
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "Click one of the buttons below for VM options:"
+                }
+              },
+              {
+                "type": "actions",
+                "elements": [
+                  {
+                    "type": "button",
+                    "text": {
+                      "type": "plain_text",
+                      "text": "List Servers"
+                    },
+                    "action_id": "button_list_servers"
+                  },
+                  {
+                    "type": "button",
+                    "text": {
+                      "type": "plain_text",
+                      "text": "Create Server"
+                    },
+                    "action_id": "button_create_vm"
+                  },
+                  {
+                    "type": "button",
+                    "text": {
+                      "type": "plain_text",
+                      "text": "Delete Server"
+                    },
+                    "action_id": "button_delete_vm"
+                  }
+                ]
+              }
+            ],
+            text: "VM options"
+          });          
     }
 }
