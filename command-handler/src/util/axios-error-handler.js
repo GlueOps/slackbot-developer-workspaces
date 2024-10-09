559bbacd0a067d2db13bb5c3c21d20cb7ed3a257
null <archulettanicholas@gmail.com>
2024-10-09 21:11:11 +0000

    fix: spelling

--- command-handler/src/util/axios-error-handler.js
+++ command-handler/src/util/axios-error-handler.js
@@ -1,7 +1,7 @@
 export default function axiosError(e) {
     const errorObj = {
-        data: e.respone ? e.response.data : null,
-        status: e.response ? e.response.status : null,
+        data: e.response?.data ?? null,
+        status: e.response?.status ?? null,
         message: e.message,
         stackTrace: e.stack
 
