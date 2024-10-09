839c39d963104672a41da9477c153a545d7697c3
null <archulettanicholas@gmail.com>
2024-10-09 15:49:06 +0000

    fix: axios error e.response undefined

--- command-handler/src/util/axios-error-handler.js
+++ command-handler/src/util/axios-error-handler.js
@@ -1,7 +1,8 @@
 export default function axiosError(e) {
     const errorObj = {
-        data: e.response.data,
-        status: e.response.status,
+        data: e.respone ? e.response.data : null,
+        status: e.response ? e.response.status : null,
+        message: e.message,
         stackTrace: e.stack
 
     }
