d1b6b4307192d455a29d485b3e3a9a663c63995f
Nicholas Archuletta <archulettanicholas@gmail.com>
2024-11-29 11:06:58 +0000

    feat: add api calls to slackbot

--- command-handler/src/util/libvirt/libvirt-server.js
+++ command-handler/src/util/libvirt/libvirt-server.js
@@ -1,16 +1,15 @@
 import logger from '../logger.js';
 import 'dotenv/config';
+import axios from 'axios';
 import tailscale from "../tailscale/tailscale.js";
 import formatUser from '../format-user.js';
 import getDevices from '../tailscale/get-devices-info.js';
 import buttonBuilder from "../button-builder.js";
-import executeSSHCommand from './ssh-connection.js';
 import configUserData from "../get-user-data.js";
-import fs from 'fs';
+import axiosError from '../axios-error-handler.js';
 import { uniqueNamesGenerator, colors, animals } from 'unique-names-generator';
 
 const log = logger();
-const nodes = JSON.parse(process.env.NODES);
 
 const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -18,7 +17,6 @@ const delay = (ms) => {
 
 export default {
     createServer: async({ app, body, imageName }) => {
-        const node = nodes[0];
         //auto generate the name
         const serverName = uniqueNamesGenerator({ 
             dictionaries: [ colors, animals ],
@@ -51,23 +49,47 @@ export default {
             user: `${body.user.id}`,
             text: `Creating the server with image: ${imageName} This will take about 5 minutes.`
         });
-        const scriptPath = '../command-handler/src/util/libvirt/create-server.sh';
-        const variables = {
-            IMAGE: imageName,
-            SERVER_NAME: serverName,
-            USER_DATA: configUserData(serverName)
-        };
-        
-        let script = fs.readFileSync(scriptPath, 'utf8');
 
-        // Replace the placeholders with actual values
-        for (const [key, value] of Object.entries(variables)) {
-            const placeholder = `{{${key}}}`;
-            script = script.replace(new RegExp(placeholder, 'g'), value);
+        let serverRes;
+        try {
+            serverRes = await axios.post(`${process.env.PROVISIONER_URL}/v1/create`, 
+                {
+                    "vm_name": serverName,
+                    "tags": {
+                        "owner": {
+                        "name": userEmail
+                        }
+                    },
+                    "user_data": configUserData(serverName),
+                    "image": imageName
+                }, {
+                headers: {
+                    'Authorization': `${process.env.PROVISIONER_API_TOKEN}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+        } catch (error) {
+            log.error('There was an error creating the server', axiosError(error));
+
+            app.client.chat.postEphemeral({
+            channel: `${body.channel.id}`,
+            user: `${body.user.id}`,
+            text: `Failed to create a server.`
+            });
+
+            return;
         }
 
-        console.log(await executeSSHCommand(node, script))
+        if (serverRes !== 'Success') {
+            app.client.chat.postEphemeral({
+            channel: `${body.channel.id}`,
+            user: `${body.user.id}`,
+            text: `Failed to create a server.`
+            });
 
+            return;
+        }
+        
         let attempts;
 
         let maxRetries = 1;  
@@ -85,28 +107,19 @@ export default {
             }
         }
 
-        // if (attempts === maxRetries) {
-        //     try {
-        //     throw new Error(`Failed to set tags in tailscale after ${attempts} retries`);
-        //     } catch (error) {
-        //         log.error({message: error.message, stack: error.stack});
-        //         app.client.chat.postEphemeral({
-        //         channel: `${body.channel.id}`,
-        //         user: `${body.user.id}`,
-        //         text: `Failed to set tags in tailscale`
-        //         });
-        //         return;
-        //     }
-        // }
-
-        // Read the JSON file
-        const data = JSON.parse(await executeSSHCommand(node, 'if [ -f /var/lib/libvirt/images/vmData.json ]; then cat /var/lib/libvirt/images/vmData.json; else echo "{}"; fi'));
-
-        // Modify the JSON data
-        data[serverName] = {Owner: userEmail}; // Add or modify your JSON data
-
-        await executeSSHCommand(node, `echo '${JSON.stringify(data)}' | sudo tee /var/lib/libvirt/images/vmData.json`);
-
+        if (attempts === maxRetries) {
+            try {
+            throw new Error(`Failed to set tags in tailscale after ${attempts} retries`);
+            } catch (error) {
+                log.error({message: error.message, stack: error.stack});
+                app.client.chat.postEphemeral({
+                channel: `${body.channel.id}`,
+                user: `${body.user.id}`,
+                text: `Failed to set tags in tailscale`
+                });
+                return;
+            }
+        }
 
         //get servers and info from tailscale
         const { deviceIP } = await getDevices(serverName);
@@ -120,7 +133,6 @@ export default {
     },
 
     deleteServer: async ({ app, body, serverName }) => {
-        const node = nodes[0];
         //get servers from tailscale
         const { deviceId } = await getDevices(serverName)
 
@@ -136,41 +148,32 @@ export default {
           });
         });
 
-        const scriptPath = '../command-handler/src/util/libvirt/remove-server.sh';
-        const variables = {
-            SERVER_NAME: serverName,
-        };
-        
-        let script = fs.readFileSync(scriptPath, 'utf8');
-
-        // Replace the placeholders with actual values
-        for (const [key, value] of Object.entries(variables)) {
-            const placeholder = `{{${key}}}`;
-            script = script.replace(new RegExp(placeholder, 'g'), value);
-        }
-
-        console.log(script)
         try {
-            console.log(await executeSSHCommand(node, script))
+            await axios.delete(`${process.env.PROVISIONER_URL}/v1/delete`, {
+                "vm_name": serverName
+            }, {
+                headers: {
+                'Authorization': `${process.env.PROVISIONER_API_TOKEN}`
+                }
+            });
+  
             app.client.chat.postEphemeral({
-            channel: `${body.channel.id}`,
-            user: `${body.user.id}`,
-            text: `Server: ${serverName} has been deleted.`
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Server: ${serverName} has been deleted.`
             });
         } catch (error) {
-          log.error('Failed to delete the server in aws', {errorStack: error.stack, message: error.message});
-
-          app.client.chat.postEphemeral({
-            channel: `${body.channel.id}`,
-            user: `${body.user.id}`,
-            text: `Failed to delete Server: ${serverName} from aws.`
-          });
+            log.error('Failed to delete the server', axiosError(error));
+  
+            app.client.chat.postEphemeral({
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Failed to delete Server: ${serverName}.`
+            });
         } 
     },
 
     listServers: async({ app, body }) => {
-        const node = nodes[0];
-        // Call the users.info method using the WebClient
         const servers = [];
         const info = await app.client.users.info({
         user: body.user.id
@@ -181,9 +184,25 @@ export default {
 
         const userEmail = formatUser(info.user.profile.email);
 
-        // Read the JSON file
-        const data = JSON.parse(await executeSSHCommand(node, 'if [ -f /var/lib/libvirt/images/vmData.json ]; then cat /var/lib/libvirt/images/vmData.json; else echo "{}"; fi'));
+        const data = await axios.get(`${process.env.PROVISIONER_URL}/v1/list`, {
+            headers: {
+              'Authorization': `${process.env.PROVISIONER_API_TOKEN}`
+            }
+          })
+          .catch(error => {
+            log.error('Failed to get servers from libvirt', axiosError(error));
+        });
 
+        if (!data) {
+            app.client.chat.postEphemeral({
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Failed to get server data from libvirt`
+            });
+
+            return [];
+        }
+    
         for (const server in data) {
             if (data.hasOwnProperty(server)) { // Check if the key belongs to the object itself
               const owner = data[server].Owner; // Access the Owner value
@@ -204,54 +223,72 @@ export default {
         return servers;
     },
 
-    startServer: async({ app, body, instanceId, region }) => {
-        const client = new EC2Client({ region });
-        const command = new StartInstancesCommand({
-            InstanceIds: [instanceId],
-        });
+    startServer: async({ app, body, serverName }) => {
         try {
-            await client.send(command);
+            await axios.post(`${process.env.PROVISIONER_URL}/v1/start`, {
+                "vm_name": serverName
+            }, {
+                headers: {
+                'Authorization': `${process.env.PROVISIONER_API_TOKEN}`
+                }
+            });
+  
             app.client.chat.postEphemeral({
-                channel: `${body.channel.id}`,
-                user: `${body.user.id}`,
-                text: `Server Id: ${instanceId} has been started.`
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Server: ${serverName} has been Started.`
             });
         } catch (error) {
-            log.error('error starting aws server', {errorStack: error.stack, message: error.message});
-        }
+            log.error('Failed to start the server', axiosError(error));
+  
+            app.client.chat.postEphemeral({
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Failed to start Server: ${serverName}.`
+            });
+        } 
     },
 
-    stopServer: async({ app, body, instanceId, region }) => {
-        const client = new EC2Client({ region });
-        const command = new StopInstancesCommand({
-            InstanceIds: [instanceId],
-        });
+    stopServer: async({ app, body, serverName }) => {
         try {
-            await client.send(command);
+            await axios.post(`${process.env.PROVISIONER_URL}/v1/stop`, {
+                "vm_name": serverName
+            }, {
+                headers: {
+                'Authorization': `${process.env.PROVISIONER_API_TOKEN}`
+                }
+            });
+  
             app.client.chat.postEphemeral({
-                channel: `${body.channel.id}`,
-                user: `${body.user.id}`,
-                text: `Server Id: ${instanceId} has been stopped.`
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Server: ${serverName} has been Stoped.`
             });
         } catch (error) {
-            log.error('error stoping aws server', {errorStack: error.stack, message: error.message});
-        }
+            log.error('Failed to stop the server', axiosError(error));
+  
+            app.client.chat.postEphemeral({
+              channel: `${body.channel.id}`,
+              user: `${body.user.id}`,
+              text: `Failed to stop Server: ${serverName}.`
+            });
+        } 
     },
 
     selectImage: async({ app, body }) => {
         //get the libvirt images
-        const images = [];
-        for (const node of nodes) {
-            try {
-                // Execute the command and trim the output
-                const output = await executeSSHCommand(node, 'ls /var/lib/libvirt/templates');
-                // Split the output by newlines and trim each line, then push the result into images
-                const trimmedImages = output.split('\n').map(image => image.trim()).filter(image => image); // Filter out empty lines
-                images.push(...trimmedImages); // Spread the trimmed array into images
-            } catch (error) {
-                log.error(`Error executing command on ${node.host}:`, error.message);
-            }
+
+        // Fetch tags from the repository
+        let images = [];
+
+        try {
+            const res = await axios.get('https://api.github.com/repos/GlueOps/codespaces/tags');
+            images = res.data.slice(0, 5).map(tag => tag.name);
+            console.log(images);
+        } catch (error) {
+            log.error('Error fetching tags:', axiosError(error));
         }
+
         const buttonsArray = [];
 
         //return if it fails to get the images.
--- command-handler/src/util/libvirt/remove-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo virsh destroy {{SERVER_NAME}}
-sleep 10
-#Remove a vm (undefine it)
-sudo virsh undefine {{SERVER_NAME}} --remove-all-storage
