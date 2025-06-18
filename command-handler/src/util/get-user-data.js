bb12ff852a9033c3d8e16554f912d82290eeceb7
Nicholas Archuletta <archulettanicholas@gmail.com>
2025-06-18 17:17:24 +0000

    fix: change user to vscode

--- command-handler/src/util/get-user-data.js
+++ command-handler/src/util/get-user-data.js
@@ -9,7 +9,7 @@ export default function configUserData(serverName) {
         hostname: ${serverName}
         manage_etc_hosts: true
         users:
-            - name: root
+            - name: vscode
               ssh_authorized_keys:
                 - ${process.env.SSH_PUBLIC_KEY}
         runcmd:
