12229a011ce340dad58185724f48b663df578c00
Nicholas Archuletta <archulettanicholas@gmail.com>
2025-06-19 19:51:26 +0000

    fix: guacamole ui

---
+++ command-handler/src/util/guacamole/guacamole.js
@@ -0,0 +1,52 @@
+import 'dotenv/config'
+import axios from 'axios'
+
+export default {
+    getConnections: async (servers = []) => {
+        const connectionType = 'c';
+        const baseUrl = process.env.GUACAMOLE_SERVER_URL;
+
+        const tokenResponse = await axios.post(`${baseUrl}/api/tokens`, 
+            {
+            "username": process.env.GUACAMOLE_SERVER_USERNAME,
+            "password": process.env.GUACAMOLE_SERVER_PASSWORD
+            }, {
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const dataSource = tokenResponse.data.dataSource;
+        const token = tokenResponse.data.authToken;
+
+        const connections = await axios.get(`${baseUrl}/api/session/data/${dataSource}/connections`, {
+            headers: {
+                "guacamole-token": `${token}`
+            }
+        });
+
+        for (const server of servers) {
+            let connectionId = null;
+            let fullUrl = null;
+            for (const connection of Object.values(connections.data)) {
+                if (connection.name === server.serverName) {
+                    connectionId = connection.identifier;
+                }
+            }
+            const rawIdentifier = `${connectionId}\0${connectionType}\0${dataSource}`;
+            const encodedString = Buffer.from(rawIdentifier, 'utf-8').toString('base64');
+            const urlSafeId = encodedString
+                .replace(/\+/g, '-')
+                .replace(/\//g, '_')
+                .replace(/=+$/, '');
+            if (!baseUrl.endsWith('/')) {
+                fullUrl = `${baseUrl}/#/client/${urlSafeId}`;
+            } else {
+                fullUrl = `${baseUrl}#/client/${urlSafeId}`;
+            }
+
+            server.connect = fullUrl;
+        }
+        return servers;
+    }
+}
